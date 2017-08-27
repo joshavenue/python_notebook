9e854f489b6d24ab3ce19d39779ae1dadf1b656f
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-27 13:13:39 +0800

    Create index_method.py

---
+++ index_method.py
@@ -0,0 +1,9 @@
+// Index method are used to find the position of the data within a list //
+
+// for example, you wanna know what position 'K' is within (name) list //
+
+name = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+
+find_k = name.index('k')
+
+print(find_k)
