498eb9db64d6a55b879b54715f51ca171e29295e
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-27 12:09:21 +0800

    Create list_join_split.py

---
+++ list_join_split.py
@@ -0,0 +1,18 @@
+// List function //
+// For example //
+name = 'John, Lewis, Clark'
+list(name) // List all the strings in an list function //
+
+print(list(name)) 
+
+// Split method //
+
+name.split() // Separates all strings into its own list //
+
+// Join method //
+
+', '.join(name) // Join new things into a list //
+
+You can join things like this way -->
+
+'Hi, my friend\'s name are '.join(name)
