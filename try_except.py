89983fc34f2ae3f99f3263c8233821d28127f324
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-28 20:26:16 +0800

    Create try_except.py

---
+++ try_except.py
@@ -0,0 +1,6 @@
+try: // Tries to run this block of code //
+    number = int(input('Enter a number'))
+except ValueError:  // If it captures input that is not an integer data //
+    print('Not a number.')
+else: // If the condition is right, it will run it //
+    print(' - ' * number)
