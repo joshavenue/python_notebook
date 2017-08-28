7e0e9554043a7b260b20f1aff47b53c855f396d3
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-28 14:28:10 +0800

    Create for_loop_1.py

---
+++ for_loop_1.py
@@ -0,0 +1,14 @@
+hellos = [
+    "Hello",
+    "Tungjatjeta",
+    "Grüßgott",
+    "Вiтаю",
+    "dobrý den",
+    "hyvää päivää",
+    "你好",
+    "早上好"
+]
+
+for x in hellos:
+  print(x, 'World')
+  
