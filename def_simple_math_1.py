eaf5bf877790e45bbc568dd1944c78b67afd1d36
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-28 19:58:53 +0800

    Create def_simple_math_1.py

---
+++ def_simple_math_1.py
@@ -0,0 +1,7 @@
+def calculate_Average(n1, n2):
+    return (n1 + n2) / 2
+
+x = int(input('Enter your first number : '))
+y = int(input('Enter your second number : '))
+
+print(calculate_Average(x,y))
