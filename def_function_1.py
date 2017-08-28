91937196b2521792248551b3d6a19ece206f1de1
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-28 15:07:55 +0800

    Create def_function_1.py

---
+++ def_function_1.py
@@ -0,0 +1,18 @@
+def check_name(x,y):
+    print(x)
+    print(y)
+
+xx = input('Word1')
+yy = input('Word2')
+
+check_name(xx,yy)
+
+def name_check(name):
+    if name.lower() == 'josh':
+        print('He da man')
+    else:
+        print('Who is that?!')
+
+zz = input('Word3')
+
+name_check(zz)
