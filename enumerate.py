05759141cd56f79e41ed668b8e0aa04f82295055
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-30 02:32:53 +0800

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,6 @@
+animals = ['cat', 'dog', 'monkey']
+for idx, animal in enumerate(animals):
+    print('#{}: {}'.format(idx + 1, animal))
+
+
+// If you want access to the index of each element within the body of a loop, use the built-in enumerate function: //
