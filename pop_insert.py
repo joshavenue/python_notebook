97192562cb5d4743a82d6154f47e8c9abaee45e4
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-02 12:21:09 +0800

    Create pop_insert.py

---
+++ pop_insert.py
@@ -0,0 +1,6 @@
+x= ["a", 2, 3, 1, False, [1, 2, 3]]     // Pop 1 from the 4th index to 0th index //
+
+# Your code goes below here
+
+y = messy_list.pop(3)                   // Pop index number 4 //
+messy_list.insert(0, y)                 // Insert it back to index number one //
