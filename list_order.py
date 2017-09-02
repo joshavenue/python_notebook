e6645fd20c01db59c18c3b70d04786f7aeb90f1a
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-02 13:24:01 +0800

    Create list_order.py

---
+++ list_order.py
@@ -0,0 +1,7 @@
+x = list(range(0,20,1)        // From 0 till 20, add 1 by 1
+
+print(x[::2])                 // Print x list in multiplication of 2
+
+print(x[2::])                 // Print x list beginning from index number 2
+
+print(x[2:6:1])               // Print x list beginning from index no. 2 till index no.6 with multiplying power of 1 //
