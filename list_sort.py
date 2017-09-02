30766be9a02470386821b098086d8f44424b9a8e
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-02 13:15:50 +0800

    Update list_sort.py

--- list_sort.py
+++ list_sort.py
@@ -2,11 +2,11 @@ x = [5,3,5,2,9,5,2,7]
 
 y = x[:]              // Duplicate the entire list without skipping
 
-z = y.sort()          // Sort its numerical order
+y.sort()          // Sort its numerical order
 
 print(x)
-print(z)
+print(y)
 
-z = [2,2,3,5,5,7,9] 
+y = [2,2,3,5,5,7,9] 
 
-z = z[2:5]            // Try this, it will print [3,5,5]
+print(z[2:5])            // Try this, it will print [3,5,5]
