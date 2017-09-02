c22feb85ca8cc2dfe6f0e50f2ba3e2105fe6a823
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-02 13:01:31 +0800

    Create list_sort.py

---
+++ list_sort.py
@@ -0,0 +1,12 @@
+x = [5,3,5,2,9,5,2,7]
+
+y = x[:]              // Duplicate the entire list without skipping
+
+z = y.sort()          // Sort its numerical order
+
+print(x)
+print(z)
+
+z = [2,2,3,5,5,7,9] 
+
+z = z[2:5]            // Try this, it will print [3,5,5]
