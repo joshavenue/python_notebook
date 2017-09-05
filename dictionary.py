89d8e6a8a422bade352d3bf94f2c59c1d0dc601b
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-05 14:13:16 +0800

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+x = {'job': 'teacher', 'color': 'blue'}   // Create a dictionary, list with defination
+
+print(x['job'])                           // You will see 'teacher'
+
+y = {'emotion': 'happy', 'reason': {'action': 'playing game', 'platform': 'PC'}}
+
+print(y['reason']['action'])              // You will see 'playing game'
