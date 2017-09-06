1f1dc94df83fae6d2019353655075cc64fd513be
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-06 12:06:04 +0800

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -4,3 +4,7 @@ y = (1,2,'abc',[1,2,3])       // Only can change data with a list, other cannot 
 y[3][1] = 22
 
 print(y)
+
+x = 1,2,3,4,5
+
+tuple(x)                      // You can create a tuple this way
