bff1da6aa894afa893047dff16f31de627a52f54
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-06 10:38:50 +0800

    Create dict_update.py

---
+++ notebook2/dict_update.py
@@ -0,0 +1,8 @@
+x = {'name': 'Bob', 'job': 'janitor', 'agent': 47}
+
+x.update({'agent': 69})                             // This will update it from 47 to 69
+
+x['weapon'] = 'pistol'                              // This will add a new key inside the dictionary
+
+del x['name']                                       // This will delete specific key
+
