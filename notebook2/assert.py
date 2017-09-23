046144734163d289d950b3a9032996908e28fa6c
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-23 18:05:55 +0800

    Create assert.py

---
+++ notebook2/assert.py
@@ -0,0 +1,7 @@
+print(0)
+assert "h" != "w"			## True, will continue to run
+print (1)
+assert False				## Since False, program stop running, displaying 1 as the highest number
+print(2)
+assert True
+print(3)
